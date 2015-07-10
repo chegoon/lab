e259fca3dced31a07db928d2de9a12ff38f902bb
chegoon <jongwon.chae@gmail.com>
2015-07-10 14:31:11 +0900

    channel index show update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+	def num_diff(num1, num2, display_flag = true)
+
+		if num1 && num2
+			ret = (num1 - num2).to_i
+		else
+			return ""
+		end
+
+		if display_flag == true && ret > 0
+			return " (+#{number_with_delimiter(ret)})"
+		elsif display_flag == true && ret < 0
+			return number_with_delimiter(ret)
+		end
+	end	
 end
