384c6879d80100f4807d7072756941aabf042146
chegoon <jongwon.chae@gmail.com>
2015-07-14 12:12:35 +0900

    channel video index show update

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,7 +4,7 @@ class VideosController < ApplicationController
   # GET /videos
   # GET /videos.json
   def index
-    @videos = Video.all
+    @videos = Video.order("published_at DESC")
   end
 
   # GET /videos/1
