abd6687f3dedef3393619ac992c8b4c29cb080f5
chegoon <jongwon.chae@gmail.com>
2015-07-16 03:31:20 +0900

    devise enabled

--- db/migrate/20150715181033_devise_create_users.rb
+++ db/migrate/20150715181033_devise_create_users.rb
@@ -3,10 +3,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
     create_table(:users) do |t|
       ## Database authenticatable
       t.string :email,  :limit => 191,  null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password, :limit => 191, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.string   :reset_password_token, :limit => 191
       t.datetime :reset_password_sent_at
 
       ## Rememberable
@@ -16,8 +16,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.integer  :sign_in_count, default: 0, null: false
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
+      t.string   :current_sign_in_ip, :limit => 191
+      t.string   :last_sign_in_ip, :limit => 191
 
       ## Confirmable
       # t.string   :confirmation_token
