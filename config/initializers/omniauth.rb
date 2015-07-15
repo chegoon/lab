7f0ebd1da43c7ab430661e6d8f307460a934934a
chegoon <jongwon.chae@gmail.com>
2015-07-16 00:15:02 +0900

    omniauth added

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	GOOGLE_CLIENT_ID = "610612216570-ti8l8mocpfjkuhsfl7m788fa0p55vqkf.apps.googleusercontent.com"
-	GOOGLE_CLIENT_SECRET = "rZ1RGSqn4EZsWjRFW4gU7SVc"
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+  provider :google_oauth2, ENV["610612216570-ti8l8mocpfjkuhsfl7m788fa0p55vqkf.apps.googleusercontent.com"], ENV["rZ1RGSqn4EZsWjRFW4gU7SVc"]
 end
