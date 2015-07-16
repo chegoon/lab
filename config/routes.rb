5ebd71d54e09b2516d3dd6ea0117b785710f9178
chegoon <jongwon.chae@gmail.com>
2015-07-16 21:05:42 +0900

    google auth updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+
+  match 'users/auth/:provider/callback', to: 'sessions#create'
+
   resources :playlist_items
 
   resources :video_statistics
