ef35e21bacad0543f15b2a90de547c763a972750
chegoon <jongwon.chae@gmail.com>
2015-07-17 22:47:22 +0900

    auth updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,11 @@ def self.new_with_session(params, session)
   end    
 end
 def self.from_omniauth(auth)
-  where(auth.slice(:provider, :uid)).first_or_create do |user|
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.provider = auth.provider
     user.uid = auth.uid
-    #user.username = auth.info.nickname
+    user.email = auth.email
+    user.name = auth.info.nickname
   end
 end
 =begin
