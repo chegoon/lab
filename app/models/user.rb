311dbd4a0084a21511045aec25af1dd4c72f54e3
chegoon <jongwon.chae@gmail.com>
2015-07-17 22:51:06 +0900

    auth updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ def self.new_with_session(params, session)
     super
   end    
 end
+=begin
 def self.from_omniauth(auth)
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.provider = auth.provider
@@ -21,19 +22,18 @@ def self.from_omniauth(auth)
     user.name = auth.info.nickname
   end
 end
-=begin
-	def self.from_omniauth(access_token)
-	    data = access_token.info
-	    user = User.where(:email => data["email"]).first
+=end
+
+	def self.from_omniauth(auth)
+	    user = User.where(:email => auth.email).first
 
 	    #Uncomment the section below if you want users to be created if they don't exist
 	     unless user
-	         user = User.create(name: data["name"],
-	            email: data["email"],
+	         user = User.create(name: auth.info.name,
+	            email: auth.email,
 	            password: Devise.friendly_token[0,20]
 	         )
 	     end
 	    user
 	end
-=end
 end
