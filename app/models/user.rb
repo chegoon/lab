ffe8bed106f6425042aed1f4aeababe14be602d0
chegoon <jongwon.chae@gmail.com>
2015-07-18 20:41:12 +0900

    auth updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ end
 	    #Uncomment the section below if you want users to be created if they don't exist
 	     unless user
 	         user = User.create(name: auth.info.name,
-	            email: auth.email,
+	            #email: auth.email,
 	            password: Devise.friendly_token[0,20]
 	         )
 	     end
