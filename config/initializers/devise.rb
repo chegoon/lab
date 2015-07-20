e0f816128c37bb132410f705f0b4750c89dee8ab
chegoon <jongwon.chae@gmail.com>
2015-07-20 12:31:41 +0900

    auth updated

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'hello@beautymeets.com'
-  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {  }
+  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], { provider_ignores_state: true }
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
